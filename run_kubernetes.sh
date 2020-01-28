4536e6926df6508df0b504de4fdef50452a5a2cf
edgar-mercado <edgar.mercado@mulesoft.com>
2020-01-27 17:08:28 -0800

    Adding final steps

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=ecme820721/udacity-ml
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run --image=$dockerpath udacity-ml --port=80 -n sre
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods -n  sre
+podname=$(kubectl get pods -o json -n sre | jq -r .items[].metadata.name)
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward $podname  8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,12 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="/udacity-ml"
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push ecme820721/udacity-ml
