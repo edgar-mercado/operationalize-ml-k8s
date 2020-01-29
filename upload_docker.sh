0e8552a54ae23c70b2e4219d71e3c521392e971b
edgar-mercado <edgar.mercado@mulesoft.com>
2020-01-28 19:56:44 -0800

    Review changes

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,12 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-dockerpath="/udacity-ml"
+dockerpath="ecme820721/udacity-ml"
 
 # Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag udacity-ml $dockerpath
 
 # Step 3:
 # Push image to a docker repository
-docker push ecme820721/udacity-ml
+docker push $dockerpath
